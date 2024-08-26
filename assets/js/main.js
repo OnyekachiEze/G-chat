dc63a05f7c9e5d24780866172dd9018e9f4fb68a
OnyekachiEze <ezeonyekachipaul@gmail.com>
2024-08-26 21:10:32 +0100

    ok

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,36 +1,35 @@
-(function() {
+(function () {
   // Responsive Sidebar Navigation - by CodyHouse.co
   var searchInput = document.getElementsByClassName('js-cd-search')[0],
     navList = document.getElementsByClassName('js-cd-nav__list')[0];
-  if( searchInput && navList) {
-    var sidebar = document.getElementsByClassName('js-cd-side-nav')[0],
+  if (searchInput && navList) {
+    var sidebar = document.getElementsByClassName('js-cd-side-nav')[0], // declare sidebar variable here
       mainHeader = document.getElementsByClassName('js-cd-main-header')[0],
       mobileNavTrigger = document.getElementsByClassName('js-cd-nav-trigger')[0],
       dropdownItems = document.getElementsByClassName('js-cd-item--has-children');
-    
     //on resize, move search and top nav position according to window width
     var resizing = false;
-    window.addEventListener('resize', function(){
-      if(resizing) return;
+    window.addEventListener('resize', function () {
+      if (resizing) return;
       resizing = true;
       (!window.requestAnimationFrame) ? setTimeout(moveNavigation, 300) : window.requestAnimationFrame(moveNavigation);
     });
     window.dispatchEvent(new Event('resize'));//trigger the moveNavigation function
 
     //on mobile, open sidebar when clicking on menu icon
-    mobileNavTrigger.addEventListener('click', function(event){
+    mobileNavTrigger.addEventListener('click', function (event) {
       event.preventDefault();
       var toggle = !Util.hasClass(sidebar, 'cd-side-nav--is-visible');
-      if(toggle) expandSidebarItem();
+      if (toggle) expandSidebarItem();
       Util.toggleClass(sidebar, 'cd-side-nav--is-visible', toggle);
       Util.toggleClass(mobileNavTrigger, 'cd-nav-trigger--nav-is-visible', toggle);
     });
 
     // on mobile -> show subnavigation on click
-    for(var i = 0; i < dropdownItems.length; i++) {
-      (function(i){
-        dropdownItems[i].children[0].addEventListener('click', function(event){
-          if(checkMQ() == 'mobile') {
+    for (var i = 0; i < dropdownItems.length; i++) {
+      (function (i) {
+        dropdownItems[i].children[0].addEventListener('click', function (event) {
+          if (checkMQ() == 'mobile') {
             event.preventDefault();
             var item = event.target.parentNode;
             Util.toggleClass(item, 'cd-side__item--expanded', !Util.hasClass(item, 'cd-side__item--expanded'));
@@ -38,18 +37,18 @@
         });
       })(i);
     }
-    
+
     //on desktop - differentiate between a user trying to hover over a dropdown item vs trying to navigate into a submenu's contents
     var listItems = sidebar.querySelectorAll('.js-cd-side__list > li');
     new menuAim({
       menu: sidebar,
-      activate: function(row) {
+      activate: function (row) {
         Util.addClass(row, 'hover');
       },
-      deactivate: function(row) {
+      deactivate: function (row) {
         Util.removeClass(row, 'hover');
       },
-      exitMenu: function() {
+      exitMenu: function () {
         hideHoveredItems();
         return true;
       },
@@ -57,13 +56,13 @@
       submenuSelector: '.js-cd-item--has-children',
     });
 
-    function moveNavigation(){ // move searchInput and navList
+    function moveNavigation() { // move searchInput and navList
       var mq = checkMQ();
-      if ( mq == 'mobile' && !Util.hasClass(navList.parentNode, 'js-cd-side-nav') ) {
+      if (mq == 'mobile' && !Util.hasClass(navList.parentNode, 'js-cd-side-nav')) {
         detachElements();
         sidebar.appendChild(navList);
         sidebar.insertBefore(searchInput, sidebar.firstElementChild);
-      } else if ( mq == 'desktop' && !Util.hasClass(navList.parentNode, 'js-cd-main-header') ) {
+      } else if (mq == 'desktop' && !Util.hasClass(navList.parentNode, 'js-cd-main-header')) {
         detachElements();
         mainHeader.appendChild(navList);
         mainHeader.insertBefore(searchInput, mainHeader.firstElementChild.nextSibling);
@@ -79,7 +78,7 @@
 
     function hideHoveredItems() { // exit sidebar -> hide dropdown
       var hoveredItems = sidebar.getElementsByClassName('hover');
-      for(var i = 0; i < hoveredItems.length; i++) Util.removeClass(hoveredItems[i], 'hover');
+      for (var i = 0; i < hoveredItems.length; i++) Util.removeClass(hoveredItems[i], 'hover');
     };
 
     function checkMQ() { // check if mobile or desktop device
@@ -92,8 +91,8 @@
 
     function checkSelected(mq) {
       // on desktop, remove expanded class from items (js-cd-item--has-children) that were expanded on mobile version
-      if( mq == 'desktop' ) {
-        for(var i = 0; i < dropdownItems.length; i++) Util.removeClass(dropdownItems[i], 'cd-side__item--expanded');
+      if (mq == 'desktop') {
+        for (var i = 0; i < dropdownItems.length; i++) Util.removeClass(dropdownItems[i], 'cd-side__item--expanded');
       };
     }
   }
